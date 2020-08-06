422eeaf7810139ef19d8458c94b17667cca854c1
Karol <anne@testingcompany.com.br>
2020-08-06 18:01:41 -0300

    ScreenShot

--- src/main/java/br/ce/wcaquino/appium/core/BaseTest.java
+++ src/main/java/br/ce/wcaquino/appium/core/BaseTest.java
@@ -1,9 +1,20 @@
 package br.ce.wcaquino.appium.core;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class BaseTest {
+	
+	@Rule
+	public TestName testName = new TestName();
 	
 	@AfterClass
 	public static void finalizaClasse() {  // tem que ser estático pq é a nivel de classe
@@ -12,7 +23,18 @@ public class BaseTest {
 
 	@After
 	public void tearDown() {
+		gerarScreenShot();
 	   DriverFactory.getDriver().resetApp();
+	}
+	
+	public void gerarScreenShot() {
+		try {
+			File imagem =((TakesScreenshot) DriverFactory.getDriver()).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(imagem, new File("target/screenshot/"+testName.getMethodName()+".png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
 
